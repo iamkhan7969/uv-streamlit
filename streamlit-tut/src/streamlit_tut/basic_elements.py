448114c46110feb875a725d5aa345d901135b7d9
HamdanDurrani <hamdanghaziofficial@gmail.com>
2025-02-22 08:38:53 +0500

    Initial commit

---
+++ streamlit-tut/src/streamlit_tut/basic_elements.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+
+st.title("Title")
+st.header("Header")
+st.subheader("Subheader")
+st.text("Text")
+st.markdown("**Markdown**")
+
+
+# to run uv run streamlit run src\streamlit_tut\basic_elements.py
