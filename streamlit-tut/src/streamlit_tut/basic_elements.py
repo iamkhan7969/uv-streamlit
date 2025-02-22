d2cd8582243ad2d77de057d147b9c64c262a8d89
HamdanDurrani <hamdanghaziofficial@gmail.com>
2025-02-22 12:56:16 +0500

    last

--- streamlit-tut/src/streamlit_tut/basic_elements.py
+++ streamlit-tut/src/streamlit_tut/basic_elements.py
@@ -1,40 +1,18 @@
 import streamlit as st
 
-st.title("To Generate a Balloon")
-st.header("Header")
-st.subheader("Subheader")
-st.subheader("This is even a subheader")
 
-s = "src/streamlit_tut/sound.mp3"  # Ensure correct path
 
 def bal():
     st.balloons()
+    
+st.chat_message(name="human")
 
-    # JavaScript for auto-playing sound
-    audio_html = f"""
-    <audio autoplay>
-        <source src="{s}" type="audio/mp3">
-    </audio>
-    """
-    st.markdown(audio_html, unsafe_allow_html=True)
-
-st.button(label="Press me", on_click=bal)
+a=st.chat_input(placeholder="Enter Your Name",on_submit=bal)
+st.title(f"Happy Birthday {a}")
+st.markdown("**------------------------------------------------------------------------------------------**")
+st.subheader("I \"Hamdan Durrani\" will wish you the best Birthday ever.")
+st.subheader("Just enter your name below")
 
 
 
-# st.title("To Generate a Ballon")
-# st.header("Header")
-# st.subheader("Suber")
-# st.subheader("This is even sub header")
-# # st.text("Text")
-# # st.markdown("**Markdown**")
-# s="src\streamlit_tut\sound.mp3"
-# def bal():
-#     st.balloons()
-#     st.audio(s)
-
-# st.
-# st.button(label="press me",on_click=bal)
-
-
-# # to run uv run streamlit run src\streamlit_tut\basic_elements.py
+# to run uv run streamlit run src\streamlit_tut\basic_elements.py
